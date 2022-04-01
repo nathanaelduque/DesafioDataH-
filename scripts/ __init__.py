55979df19512fef349e2ea55dcc3587f6c5689e6
Nathanael Duque <nathanael.duque.gadelha@gmail.com>
2022-04-01 01:54:22 -0300

    Delete scripts directory

--- scripts/ __init__.py
+++
@@ -1,7 +0,0 @@
-from .check_PokerHand import *
-from .count_score import *
-from .DefResult import *
-from .checkpairs import *
-from .royal import *
-from .untie_case import *
-from .compare_hands import * 
